e53df061193e8bc294e58e0f013a3b49905abccc
virginiacastellano <vcastellano140@gmail.com>
2024-07-03 20:57:03 -0300

    se modifica la app plataforma para poder agregar las nuevas app,participante,proyecto,ediccion,modulo,celula

--- plataforma/plataforma/urls.py
+++ plataforma/plataforma/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/usuarios/', include('usuarios.urls')),
+    path("admin/", admin.site.urls),
+    path("api/usuarios/", include("usuarios.urls")),
+    path("api/participante/", include("participante.urls")),
+    path("api/proyecto/", include("proyecto.urls")),
+    path("api/edicion/", include("edicion.urls")),
+    path("api/celula/", include("celula.urls")),
+    path("api/modulo/", include("modulo.urls")),
 ]
